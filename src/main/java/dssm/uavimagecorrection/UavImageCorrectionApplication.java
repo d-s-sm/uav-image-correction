476a30b76c8aa6ee2180f38e57c6bae5e5348e96
Dmitrii Smirnov <d.s.sm@ya.ru>
2022-11-03 16:48:13 +0700

    Created ClassDiagram-file

--- src/main/java/dssm/uavimagecorrection/UavImageCorrectionApplication.java
+++ src/main/java/dssm/uavimagecorrection/UavImageCorrectionApplication.java
@@ -3,11 +3,17 @@ package dssm.uavimagecorrection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class UavImageCorrectionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UavImageCorrectionApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(UavImageCorrectionApplication.class, args);
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
